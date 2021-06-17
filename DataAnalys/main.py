d545eaa25c53efce3079e5ba954c8097e673032c
Navid Otadi <navid.otadi@gmail.com>
2021-06-17 21:12:04 +0200

    add circle maping for prime

--- DataAnalys/main.py
+++ DataAnalys/main.py
@@ -4,7 +4,7 @@ import numpy
 
 def PlotMulti(pL):
         for p in pL:
-            Plot(p)
+           1+1 #Plot(p)
 
 def main():
 
@@ -14,18 +14,22 @@ def main():
          info.append( math.sin( r ))
     #Plot(info)
     infoNrml= [element/max(info) * 100 for element in info]
-    
+    infoNrml= range(0, 20)
+    #------------
+    infoNrml= generateNumber()
+
+    #------------
     dataAry=[]
     dataAry.append(infoNrml)
     responseAry=[]
     x=0
     pdfSeedList = []
-    while x<2 :
+    while x<3 :
         for  infoNrml in dataAry:
             l=len(infoNrml)
             l=len(dataAry)
             pdfResult = Pdf(infoNrml,2,x)
-            for ary in  pdfResult.Values  :
+            for ary in  pdfResult.Values :
                 responseAry.append(ary)                           
             pdfSeedList.append(pdfResult)
         dataAry=responseAry
