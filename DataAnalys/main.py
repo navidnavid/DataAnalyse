d2520c89f2f9a74fbc9417cd496670bc5293c51c
Navid Otadi <navid.otadi@gmail.com>
2021-06-18 21:05:54 +0200

    add empty array handling

--- DataAnalys/main.py
+++ DataAnalys/main.py
@@ -2,9 +2,9 @@ from Tools.Plot import *
 import math 
 import numpy
 
-def PlotMulti(pL):
-        for p in pL:
-           1+1 #Plot(p)
+#def PlotMulti(pL):
+#        for p in pL:
+#            Plot(p)
 
 def main():
 
@@ -22,18 +22,21 @@ def main():
     dataAry=[]
     dataAry.append(infoNrml)
     responseAry=[]
+    PrbOccurence=[]
     x=0
     pdfSeedList = []
-    while x<3 :
+    while x<10 :
         for  infoNrml in dataAry:
             l=len(infoNrml)
             l=len(dataAry)
             pdfResult = Pdf(infoNrml,2,x)
+            PrbOccurence.append(pdfResult.Ocurrence)
             for ary in  pdfResult.Values :
-                responseAry.append(ary)                           
+                responseAry.append(ary)
             pdfSeedList.append(pdfResult)
-        dataAry=responseAry
-        PlotMulti(responseAry)
+        responseAryNotEmpty = [x for x in responseAry if (x != [] and len(x)>1)]
+        dataAry=responseAryNotEmpty
+        #PlotMulti(responseAry) #add epty remove 
         responseAry=[]
         x =x+1
     
