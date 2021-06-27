13184ff51e584c2fe39cd10771cbbddae0163b59
Navid Otadi <navid.otadi@gmail.com>
2021-06-27 12:42:01 +0200

    Add comments

--- DataAnalys/main.py
+++ DataAnalys/main.py
@@ -4,7 +4,7 @@ import numpy
 
 #the idea is the pdf shape indicates info or relations -> then can changed saped into level help predict the next elements ?
 
-def main():
+def main(): # This project should be called find a distribution model! (: 
 
     info=[]
 
@@ -19,8 +19,8 @@ def main():
     PrbOccurence=[]
     level=0
     pdfSeedList = []
-    SeedQuanity=100
-    while level<4:
+    SeedQuanity=20
+    while level<2:
         for  infoNrml in dataAry:
             l=len(infoNrml)
             l=len(dataAry)
@@ -33,7 +33,8 @@ def main():
         dataAry=responseAryNotEmpty
 
         #Plot(PrbOccurence, 2)
-        selectedAry = list(dict.fromkeys([0, round(len(PrbOccurence)/2), len(PrbOccurence)-1]))
+        #selectedAry = list(dict.fromkeys([0, round(len(PrbOccurence)/2), len(PrbOccurence)-1]))
+        selectedAry = list(dict.fromkeys([0, 1, 2]))
         PlotByIdx(PrbOccurence, selectedAry)
         #Plot(responseAryNotEmpty)
         #PlotMulti(responseAry) #add epty remove 
@@ -46,4 +47,7 @@ def main():
 if __name__ == "__main__":
     main()
 
-
+    #todo do -> the goal is to find relation in each level which can decrease the enthropy tp zero.
+    # then it is like knowing by features (algebra)-> which properties can be find in each level 
+    # Also it is ajusta tet åproject for python this is why this algorithm is vague and there is no
+    # value continuig it. 
